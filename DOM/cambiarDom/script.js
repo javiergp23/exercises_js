cfe1d67b4525805b0d65f29ea68d72387d3d7b77
Javier Gonzalez <javgon2306@gmail.com>
2024-08-14 22:36:01 -0300

    form

--- DOM/cambiarDom/script.js
+++ DOM/cambiarDom/script.js
@@ -5,4 +5,6 @@ document.getElementById('entradaUno').value = "Click Aqui para ser hackeado";
 document.getElementById('entradaUno').type = 'button';
 
 document.getElementById('entradaUno').setAttribute("style", "background-color: yellow");
-document.getElementById('entradaUno').setAttribute(onclick, "alert('Hola este parrafo fue hackeado')");
+document.getElementById('entradaUno').setAttribute("onClick", "window.location.href='http://www.google.com'");
+document.getElementById('entradaUno').setAttribute("unmovingmouse", "false", alert("fuiste hackeado"));
+
